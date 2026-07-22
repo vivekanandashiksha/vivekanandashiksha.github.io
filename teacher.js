b697dc1f52575dc45cb335a9f34975d707d7da86
vivekanandashiksha <vivekanandashiksha0508@gmail.com>
2026-07-22 12:40:54 +0530

    Update teacher.js

--- teacher.js
+++ teacher.js
@@ -1,24 +1,14 @@
 function loginTeacher(){
-
-let pass=document.getElementById("password").value;
-
-if(pass=="dhruv058605remo"){
-
-window.location="teacher-dashboard.html";
-
-}
-
-else{
-
-document.getElementById("msg").innerHTML=
-"😄 Abhi baccha hai...<br>Ja re Student Portal me.";
-
-setTimeout(function(){
-
-window.location="student.html";
-
-},2500);
-
-}
-
+  let pass=document.getElementById("password").value;
+  if(pass=="dhruv058605remo"){
+    sessionStorage.setItem("teacherLoggedIn","true");
+    window.location="teacher-dashboard.html";
+  }
+  else{
+    document.getElementById("msg").innerHTML=
+    "😄 Abhi baccha hai...<br>Ja re Student Portal me.";
+    setTimeout(function(){
+      window.location="student.html";
+    },2500);
+  }
 }
